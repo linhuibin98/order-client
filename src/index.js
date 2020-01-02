a00c83726e857b9788b0778d90ca356feceabe56
lhb1032352370 <1032352370@qq.com>
2020-01-02 16:13:08 +0800

    浏览器router

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import store from './store';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <Router>
